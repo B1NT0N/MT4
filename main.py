f9aa8d16de0b289d7a4584b6d7b063cdf22a5048
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-12-21 22:27:51 -0100

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import time
+import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+api_token = os.getenv('API_TOKEN')
+account_id = os.getenv('ACCOUNT_ID')
+server_url = 'https://mt-client-api-v1.agiliumtrade.agiliumtrade.ai'
+
+## Methods
+
+###Returns account information for a specified MetaTrader account
+def get_account_info(api_token,account_id,):
+    
+    headers={'auth-token':api_token}
+    account_info = requests.get(f"{server_url}/users/current/accounts/{account_id}/accountInformation?{api_token}",headers=headers)
+    
+    return account_info
+
+##Returns account information for a specified MetaTrader account
+def get_positions_info(api_token,account_id,):
+    
+    headers={'auth-token':api_token}
+    positions_info = requests.get(f"{server_url}/users/current/accounts/{account_id}/positions?{api_token}",headers=headers)
+    
+    return positions_info
+
+get_positions_info(api_token,account_id)
+print(json.dumps(positions_info.json(),indent=2))
