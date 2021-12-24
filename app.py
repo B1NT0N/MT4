e1b1815fc6f34b37fb3c52d869da402f60d48e52
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-12-24 15:40:09 -0100

    New Structure but Still Static

--- app.py
+++ app.py
@@ -1,94 +1,100 @@
 import tkinter as tk
-from tkinter import ttk
-import main
+from metaapi_cloud_sdk import MetaStats, MetaApi
+import asyncio
+import time
 import os
 import json
 import requests
+import main
 from dotenv import load_dotenv
-
+from tkinter import messagebox
 load_dotenv()
 
 class App():
 
     def __init__(self,master=None):
         
+        self.account_info=None
+        self.positions_info=None
         self.profit=0.0
-        
+        # your MetaApi API token
         self.api_token = os.getenv('API_TOKEN')
+        # your MetaApi account id
         self.account_id = os.getenv('ACCOUNT_ID')
-        self.account_info = main.get_account_info(self.api_token, self.account_id)
-        self.positions_info = main.get_positions_info(self.api_token, self.account_id)
+        # your MetaApi account
+        self.account = None
         
+        self.label_balance = tk.Label(root, text ="",
+                                        font=('Helvetica', 10),
+                                        fg='White',
+                                        bg='#1e1e1e',
+                                        padx=8,)
+        self.label_balance.grid(row = 0, column = 0, sticky = 'W',padx=2,pady=2)
         
-        self.label_balance = tk.Label(root, text =f"Balance --------------->{self.account_info['balance']}",
-                                    font=('Helvetica', 10),
-                                    fg='White',
-                                    bg='#1e1e1e',
-                                    padx=8,
-                                    
-                                    ).grid(row = 0, column = 0, sticky = 'W',padx=2,pady=2)
-        self.label_equity = tk.Label(root, text =f"Equity ----------------->{self.account_info['equity']}",
-                                    font=('Helvetica', 10),
-                                    fg='White',
-                                    bg='#1e1e1e',
-                                    padx=8,
-                                    
-                                    ).grid(row = 1, column = 0, sticky = 'W',padx=2,pady=2)
-        self.label_free_margin = tk.Label(root, text =f"Free Margin --------->{self.account_info['freeMargin']}",
-                                    font=('Helvetica', 10),
-                                    padx=8,
-                                    fg='White',
-                                    bg='#1e1e1e',
-                                    ).grid(row = 2, column = 0, sticky = 'W',padx=2,pady=2)
-
-        if len(self.positions_info)!=0:
-            for position in self.positions_info:
-                self.profit+=position['profit']
-        else:
-            self.profit=0.0
-
-        self.label_total = tk.Label(root,
-                                    text =f"{self.profit}$",
-                                    font=('Helvetica', 20),
-                                    fg=f"{'Green' if (self.profit>0) else 'Red'}",
-                                    bg='#1e1e1e'
-                                    ).grid(row = 0, column = 1, rowspan = 3,padx=5,pady=10)
+        self.label_equity = tk.Label(root, text ="",
+                                        font=('Helvetica', 10),
+                                        fg='White',
+                                        bg='#1e1e1e',
+                                        padx=8)
+        self.label_equity.grid(row = 1, column = 0, sticky = 'W',padx=2,pady=2)
         
-        self.positions_info = main.get_positions_info(self.api_token, self.account_id)
+        self.label_free_margin = tk.Label(root, text ="",
+                                        font=('Helvetica', 10),
+                                        padx=8,
+                                        fg='White',
+                                        bg='#1e1e1e')
+        self.label_free_margin.grid(row = 2, column = 0, sticky = 'W',padx=2,pady=2)
+        
+        self.label_total = tk.Label(root, text ="",
+                                        font=('Helvetica', 20),
+                                        fg=f"{'Green' if (self.profit>0) else 'Red'}",
+                                        bg='#1e1e1e')
+        self.label_total.grid(row = 0, column = 1, rowspan = 3,padx=5,pady=10)
+        
         self.listbox = tk.Listbox(root,
-                                  height=18,
-                                  width=45,
-                                #   activestyle = 'NONE',
-                                  bg='#252526',
-                                  bd=0,
-                                  highlightcolor='#3e3e42',
-                                  selectbackground='#007acc'
-                                  )
-
-        if len(self.positions_info)!=0:
-            for position in self.positions_info:
-                self.listbox.insert('end', 
-                                    f"{position['symbol']} : {'SELL' if (position['type'] == 'POSITION_TYPE_SELL') else 'BUY'}                                                      {position['profit']}",
+                                    height=18,
+                                    width=45,
+                                    bg='#252526',
+                                    bd=0,
+                                    highlightcolor='#3e3e42',
+                                    selectbackground='#007acc'
                                     )
-                self.listbox.itemconfig("end",
-                                        bg=f"{'Green' if (position['profit']>0)else 'Red'}",
-                                        fg=f"{'Black' if (position['profit']>0)else 'White'}")
-        else:
-            self.listbox.insert('end', "Null")
-        
         self.listbox.grid(row = 3, column = 0, columnspan = 3,padx=13,pady=10)
         
-        def update(self):
+    def update_data(self):
+        self.account_info = main.get_account_info(self.api_token,self.account_id)
+        self.positions_info = main.get_positions_info(self.api_token,self.account_id)
+        
+        try:
+            self.label_balance.config(text=f"Balance --------------->{format(self.account_info['balance'],'.2f')}")
             
-            self.account_info = main.get_account_info(self.api_token, self.account_id)
-            self.positions_info = main.get_positions_info(self.api_token, self.account_id)
+            self.label_equity.config(text=f"Equity ----------------->{format(self.account_info['equity'],'.2f')}")
             
-       
+            self.label_free_margin.config(text=f"Free Margin --------->{format(self.account_info['freeMargin'],'.2f')}")
+            
+            if len(self.positions_info)!=0:
+                for position in self.positions_info:
+                    self.profit+=position['profit']
+            else:
+                self.profit=0.0
+
+            self.label_total.config(text =f"{format(self.profit,'.2f')}$")
+            
+            if len(self.positions_info)!=0:
+                for position in self.positions_info:
+                    self.listbox.insert('end', 
+                                        f"{position['symbol']} : {'SELL' if (position['type'] == 'POSITION_TYPE_SELL') else 'BUY'}                                                      {format(position['profit'],'.2f')}",
+                                        )
+                    self.listbox.itemconfig("end",
+                                            bg=f"{'Green' if (position['profit']>0)else 'Red'}",
+                                            fg=f"{'Black' if (position['profit']>0)else 'White'}")
+            else:
+                self.listbox.insert('end', "Null")
+            
+        except Exception as error:
+                tk.messagebox.showerror('error', f'{self.account_info["error"]}')
         
-        
-        
-        
-    
+                
 
 root = tk.Tk()
 
@@ -97,5 +103,7 @@ root.resizable(False,False)
 root.iconbitmap("images/mt4.ico")
 root.title("MT4 View")
 root.configure(bg='#1e1e1e')
-App(root)
+
+app = App(root)
+root.after(6000,app.update_data)
 root.mainloop()
